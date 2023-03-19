37d68849f70c66ec8276547f4ff7f80bde81ba55
andresaftari <andresaftari@gmail.com>
2023-03-19 23:41:59 +0700

    Some algorithm

---
+++ out/production/ForTeaching/complexity/sa/range_finder.py
@@ -0,0 +1,28 @@
+import math
+
+
+def __rangefinder__(x: list[int], y: list[int]):
+    x1, y1, x2, y2 = 0, 0, 0, 0
+
+    min_range = math.sqrt((x[0] - x[1]) * (x[0] - x[1]) + (y[0] - y[1]) * (y[0] - y[1]))
+
+    for i in range(0, len(y) - 1):
+        for j in range(i + 1, len(y)):
+            print(j)
+            known_range = math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]))
+
+            if known_range < min_range:
+                min_range = known_range
+                x1 = x[i]
+                x2 = x[j]
+                y1 = y[i]
+                y2 = y[j]
+
+    print('min range:', min_range, 'at:', {x1, y1}, 'and', {x2, y2})
+
+
+if __name__ == '__main__':
+    c_x = [2, 4, 6, 7, 8]
+    c_y = [2, 4, 9, 5, 4]
+
+    __rangefinder__(c_x, c_y)
