ef95c773af324d58b2c383bf1f7c2be713fa64bb
andresaftari <andresaftari@gmail.com>
2024-03-07 11:16:36 +0700

    Pyna Dart

--- src/ajarinPynaaDart/mod3/soal2.dart
+++ src/ajarinPynaaDart/mod3/soal2.dart
@@ -1,3 +1,6 @@
+// kalo mau pake input, uncomment import ini
+// import 'dart:io';
+
 class Balok {
   final int panjang, lebar, tinggi;
 
@@ -15,6 +18,15 @@ class Balok {
 }
 
 void main() {
+  // Contoh pake input (uncomment 6 baris di bawah)
+  // int? p = int.parse(stdin.readLineSync()!);
+  // int? l = int.parse(stdin.readLineSync()!);
+  // int? t = int.parse(stdin.readLineSync()!);
+  
+  // Balok balok = Balok(p, l, t);
+  // balok.hitungLuas();
+  // balok.hitungVolume();
+
   print('\ncontoh ketika p = 13, l = 7, t = 5');
   Balok balok1 = Balok(13, 7, 5);
   balok1.hitungLuas();
